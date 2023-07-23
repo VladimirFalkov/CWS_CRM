b70e5d1d40774f5bddb135ed35d9e66590339d9c
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-07-23 13:23:11 +0300

    Fixed styling for login and signup

--- tealcrm/urls.py
+++ tealcrm/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views
 from django.urls import path, include
 
 from core.views import index, about
+from userprofile.forms import LoginForm
 
 urlpatterns = [
     path('', index, name='index'),
@@ -17,7 +18,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("about/", about, name='about'),
     
-    path('login/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('login/', views.LoginView.as_view(template_name='userprofile/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.decorators import login_required
-
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+from .forms import SignupForm
 from .models import Userprofile
 from team.models import Team
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
 
         if form.is_valid():
             user = form.save()
@@ -18,7 +17,7 @@ def signup(request):
             team.save()
             return redirect('/log-in/')
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'userprofile/signup.html', {
         'form': form
     })
