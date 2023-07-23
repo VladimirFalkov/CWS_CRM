ccd5d6703c3bdbbe468aab7ee3ff2f005ed603e1
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-07-23 12:49:28 +0300

    detail page with member list in team app. Base page some changes

--- team/urls.py
+++ team/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import edit_team
+from .views import edit_team, detail
 
 
 app_name = 'team'
 
 urlpatterns = [
+    path('<int:pk>/', detail, name='detail'),
     path('<int:pk>/edit/', edit_team, name='edit'),
 ]
